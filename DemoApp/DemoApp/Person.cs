a9be1b5dda277682ced7c053fc691ada92a34ef3
ShimuSourav <rdfox.007@gmail.com>
2019-11-05 00:31:17 +0600

    hands on

---
+++ DemoApp/DemoApp/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    class Person
+    {
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+
+    }
+}
---
+++ DemoApp/DemoApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            string firstName = "James";
+            string lastName = "Leo";
+
+            person.FirstName = firstName;
+            person.LastName = lastName;
+             
+            string fullName = person.GetFullName();
+            Console.WriteLine(fullName);
+
+            Console.ReadKey();
+        }
+    }
+}
